fb32505fd10fff035f90807628be204c054f571c
ibrahimgltkn <ibrhm_gltkn74@hotmail.com>
2024-01-24 02:23:32 +0300

    Mediator CQRS FooterAddress eklendi.

---
+++ Core/CarBook.Application/Features/Mediator/Handleres/FooterAddressHandlers/UpdateFooterAddressCommandHandler.cs
@@ -0,0 +1,32 @@
+﻿using CarBook.Application.Features.Mediator.Commands.FooterAddressCommands;
+using CarBook.Application.Interfaces;
+using CarBook.Domain.Entities;
+using MediatR;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CarBook.Application.Features.Mediator.Handleres.FooterAddressHandlers
+{
+    public class UpdateFooterAddressCommandHandler : IRequestHandler<UpdateFooterAddressCommand>
+    {
+        private readonly IRepository<FooterAddress> _repository;
+
+        public UpdateFooterAddressCommandHandler(IRepository<FooterAddress> repository)
+        {
+            _repository = repository;
+        }
+
+        public async Task Handle(UpdateFooterAddressCommand request, CancellationToken cancellationToken)
+        {
+            var value = await _repository.GetByIdAsync(request.FooterAddressID);
+            value.Description = request.Description;
+            value.Phone = request.Phone;
+            value.Address = request.Address;
+            value.Email = request.Email;
+            await _repository.UpdateAsync(value);
+        }
+    }
+}
---
+++ Presentation/CarBook.WebApi/Controllers/FooterAddressesController.cs
@@ -0,0 +1,57 @@
+﻿using CarBook.Application.Features.Mediator.Commands.FooterAddressCommands;
+using CarBook.Application.Features.Mediator.Queries.FooterAddressQueries;
+using MediatR;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace CarBook.WebApi.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class FooterAddressesController : ControllerBase
+    {
+        private readonly IMediator _mediator;
+
+        public FooterAddressesController(IMediator mediator)
+        {
+            _mediator = mediator;
+        }
+
+        [HttpGet]
+        public async Task<IActionResult>  FooterAddressesList()
+        {
+            var values = await _mediator.Send(new GetFooterAddressQuery());
+            return Ok(values);
+        }
+
+        [HttpGet("{id}")]
+        public async Task<IActionResult> GetFooterAddresses(int id)
+        {
+            var values = await _mediator.Send(new GetFooterAddressByIdQuery(id));
+            return Ok(values);
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> CreateFooterAddresses(CreateFooterAddressCommand command)
+        {
+            await _mediator.Send(command);
+            return Ok("Footer bilgisi eklendi.");
+        }
+
+        [HttpDelete]
+        public async Task<IActionResult> RemoveFooterAddresses(int id)
+        {
+            await _mediator.Send(new RemoveFooterAddressCommand(id));
+            return Ok("Footer bilgisi silindi.");
+        }
+
+        [HttpPut]
+        public async Task<IActionResult> UpdateFooterAddresses(UpdateFooterAddressCommand command)
+        {
+            await _mediator.Send(command);
+            return Ok("Footer bilgisi güncellendi.");
+        }
+
+
+    }
+}
