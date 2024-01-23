16326d7f592f1f4c97153677ffe6cf578152aa71
ibrahimgltkn <ibrhm_gltkn74@hotmail.com>
2024-01-24 01:00:42 +0300

    Mediator Service dosyası eklendi.

--- Presentation/CarBook.WebApi/Program.cs
+++ Presentation/CarBook.WebApi/Program.cs
@@ -6,6 +6,7 @@ using CarBook.Application.Features.CQRS.Handlers.CategoryHandler;
 using CarBook.Application.Features.CQRS.Handlers.ContactHandler;
 using CarBook.Application.Interfaces;
 using CarBook.Application.Interfaces.CarInterfaces;
+using CarBook.Application.Services;
 using CarBook.Persistence.Context;
 using CarBook.Persistence.Repositories;
 using CarBook.Persistence.Repositories.CarRepositories;
@@ -54,6 +55,7 @@ builder.Services.AddScoped<CreateContactCommandHandler>();
 builder.Services.AddScoped<UpdateContactCommandHandler>();
 builder.Services.AddScoped<RemoveContactCommandHandler>();
 
+builder.Services.AddApplicationService(builder.Configuration);
 
 // Add services to the container.
 
