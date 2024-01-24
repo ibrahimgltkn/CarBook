35c8451fcdb52ecbc8fd5acc2e253818581fc1b1
ibrahimgltkn <ibrhm_gltkn74@hotmail.com>
2024-01-25 01:47:05 +0300

    Mediator CQRS Location ve Pricing eklendi.

---
+++ Core/CarBook.Application/Features/Mediator/Handleres/LocationHandlers/GetLocationByIdQueryHandler.cs
@@ -0,0 +1,33 @@
+﻿using CarBook.Application.Features.Mediator.Queries.LocationQueries;
+using CarBook.Application.Features.Mediator.Results.LocationResults;
+using CarBook.Application.Interfaces;
+using CarBook.Domain.Entities;
+using MediatR;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CarBook.Application.Features.Mediator.Handleres.LocationHandlers
+{
+    public class GetLocationByIdQueryHandler : IRequestHandler<GetLocationByIdQuery, GetLocationByIdQueryResult>
+    {
+        private readonly IRepository<Location> _repository;
+
+        public GetLocationByIdQueryHandler(IRepository<Location> repository)
+        {
+            _repository = repository;
+        }
+
+        public async Task<GetLocationByIdQueryResult> Handle(GetLocationByIdQuery request, CancellationToken cancellationToken)
+        {
+            var value = await _repository.GetByIdAsync(request.Id);
+            return new GetLocationByIdQueryResult
+            {
+                LocationID = value.LocationID,
+                Name = value.Name
+            };
+        }
+    }
+}
---
+++ Core/CarBook.Application/Features/Mediator/Handleres/LocationHandlers/RemoveLocationCommandHandler.cs
@@ -0,0 +1,28 @@
+﻿using CarBook.Application.Features.Mediator.Commands.LocationCommands;
+using CarBook.Application.Interfaces;
+using CarBook.Domain.Entities;
+using MediatR;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CarBook.Application.Features.Mediator.Handleres.LocationHandlers
+{
+    public class RemoveLocationCommandHandler : IRequestHandler<RemoveLocationCommand>
+    {
+        private readonly IRepository<Location> _repository;
+
+        public RemoveLocationCommandHandler(IRepository<Location> repository)
+        {
+            _repository = repository;
+        }
+
+        public async Task Handle(RemoveLocationCommand request, CancellationToken cancellationToken)
+        {
+            var value = await _repository.GetByIdAsync(request.Id);
+            await _repository.RemoveAsync(value);
+        }
+    }
+}
