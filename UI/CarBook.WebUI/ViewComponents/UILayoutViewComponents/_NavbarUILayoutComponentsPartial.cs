c6d6bf11ab872a04e7c297ecddaea201d7688330
ibrahimgltkn <ibrhm_gltkn74@hotmail.com>
2024-01-25 16:57:37 +0300

    UI Layout oluşturuldu.

---
+++ UI/CarBook.WebUI/ViewComponents/UILayoutViewComponents/_NavbarUILayoutComponentsPartial.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace CarBook.WebUI.ViewComponents.UILayoutViewComponents
+{
+    public class _NavbarUILayoutComponentsPartial : ViewComponent
+    {
+        public IViewComponentResult Invoke()
+        {
+            return View();
+        }
+    }
+}
