95e196404873d0a721c6c0df0829a7d7bc45da52
ibrahimgltkn <ibrhm_gltkn74@hotmail.com>
2024-01-27 03:55:11 +0300

    Araç Listesi UI Oluşturuldu.

---
+++ UI/CarBook.WebUI/Controllers/CarController.cs
@@ -0,0 +1,29 @@
+﻿using CarBook.Dto.CarDtos;
+using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json;
+
+namespace CarBook.WebUI.Controllers
+{
+    public class CarController : Controller
+    {
+        private IHttpClientFactory _httpClientFactory;
+
+        public CarController(IHttpClientFactory httpClientFactory)
+        {
+            _httpClientFactory = httpClientFactory;
+        }
+
+        public async Task<IActionResult> Index()
+        {
+            var client = _httpClientFactory.CreateClient();
+            var responseMessage = await client.GetAsync("https://localhost:7208/api/Cars/GetCarWithBrand");
+            if (responseMessage.IsSuccessStatusCode)
+            {
+                var jsonData = await responseMessage.Content.ReadAsStringAsync();
+                var values = JsonConvert.DeserializeObject<List<ResultCarWithBrandsDtos>>(jsonData);
+                return View(values);
+            }
+            return View();
+        }
+    }
+}
