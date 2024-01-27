7c76d5a883423d60a94fa83e79a54f6d36b14b72
ibrahimgltkn <ibrhm_gltkn74@hotmail.com>
2024-01-27 03:07:49 +0300

    Hakkımızda sayfası UI oluşturuldu.

---
+++ UI/CarBook.Dto/TestimonialsDtos/ResultTestimonialDto.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CarBook.Dto.TestimonialsDtos
+{
+    public class ResultTestimonialDto
+    {
+        public int testimonialID { get; set; }
+        public string name { get; set; }
+        public string title { get; set; }
+        public string comment { get; set; }
+        public string imageUrl { get; set; }
+
+    }
+}
---
+++ UI/CarBook.WebUI/ViewComponents/TestimonialViewComponents/_TestimonialComponentPartial.cs
@@ -0,0 +1,29 @@
+﻿using CarBook.Dto.TestimonialsDtos;
+using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json;
+
+namespace CarBook.WebUI.ViewComponents.TestimonialViewComponents
+{
+    public class _TestimonialComponentPartial : ViewComponent
+    {
+        private readonly IHttpClientFactory _httpClientFactory;
+
+        public _TestimonialComponentPartial(IHttpClientFactory httpClientFactory)
+        {
+            _httpClientFactory = httpClientFactory;
+        }
+
+        public async Task<IViewComponentResult> InvokeAsync()
+        {
+            var client = _httpClientFactory.CreateClient();
+            var responseMessage = await client.GetAsync("https://localhost:7208/api/Testimonials");
+            if (responseMessage.IsSuccessStatusCode)
+            {
+                var jsondata = await responseMessage.Content.ReadAsStringAsync();
+                var values = JsonConvert.DeserializeObject<List<ResultTestimonialDto>>(jsondata);
+                return View(values);
+            }
+            return View();
+        }
+    }
+}
