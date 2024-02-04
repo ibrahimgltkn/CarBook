8d8e5e51a4566126658ad64e8af1ef4114f47d81
ibrahimgltkn <ibrhm_gltkn74@hotmail.com>
2024-02-05 02:02:41 +0300

    Blog Detay Etiket Bulutu listelenmesi.

--- UI/CarBook.WebUI/ViewComponents/BlogViewComponents/_BlogDetailTagCloudComponentPartial.cs
+++ UI/CarBook.WebUI/ViewComponents/BlogViewComponents/_BlogDetailTagCloudComponentPartial.cs
@@ -16,12 +16,13 @@ namespace CarBook.WebUI.ViewComponents.BlogViewComponents
 
         public async Task<IViewComponentResult> InvokeAsync(int id)
         {
+            ViewBag.BlogID = id;
             var client = _httpClientFactory.CreateClient();
-            var responseMessage = await client.GetAsync($"https://localhost:7208/api/TagClouds/" + id);
+            var responseMessage = await client.GetAsync($"https://localhost:7208/api/TagClouds/GetTagCloudByBlogId?id=" + id);
             if (responseMessage.IsSuccessStatusCode)
             {
                 var jsondata = await responseMessage.Content.ReadAsStringAsync();
-                var values = JsonConvert.DeserializeObject<ResultTagCloudByIdDto>(jsondata);
+                var values = JsonConvert.DeserializeObject<List<ResultTagCloudByIdDto>>(jsondata);
                 return View(values);
             }
             return View();
