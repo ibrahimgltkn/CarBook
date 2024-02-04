bbcbf77e174df4a67daa178064e647a6402f7f50
ibrahimgltkn <ibrhm_gltkn74@hotmail.com>
2024-02-04 15:49:17 +0300

    Blog Detay Kategori ve Son Blogları UI Listelenmesi.

--- UI/CarBook.WebUI/ViewComponents/BlogViewComponents/_BlogDetailCategoryComponentPartial.cs
+++ UI/CarBook.WebUI/ViewComponents/BlogViewComponents/_BlogDetailCategoryComponentPartial.cs
@@ -1,11 +1,28 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using CarBook.Dto.CategoryDtos;
+using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json;
 
 namespace CarBook.WebUI.ViewComponents.BlogViewComponents
 {
     public class _BlogDetailCategoryComponentPartial : ViewComponent
     {
-        public IViewComponentResult Invoke()
+        private readonly IHttpClientFactory _httpClientFactory;
+
+        public _BlogDetailCategoryComponentPartial(IHttpClientFactory httpClientFactory)
         {
+            _httpClientFactory = httpClientFactory;
+        }
+
+        public async Task<IViewComponentResult> InvokeAsync()
+        {
+            var client = _httpClientFactory.CreateClient();
+            var responseMessage = await client.GetAsync("https://localhost:7208/api/Categories");
+            if (responseMessage.IsSuccessStatusCode)
+            {
+                var jsonData = await responseMessage.Content.ReadAsStringAsync();
+                var values = JsonConvert.DeserializeObject<List<ResultCategoryDto>>(jsonData);
+                return View(values);
+            }
             return View();
         }
     }
--- UI/CarBook.WebUI/ViewComponents/BlogViewComponents/_BlogDetailRecentBlogsComponentPartial.cs
+++ UI/CarBook.WebUI/ViewComponents/BlogViewComponents/_BlogDetailRecentBlogsComponentPartial.cs
@@ -1,12 +1,31 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using CarBook.Dto.BlogDtos;
+using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json;
 
 namespace CarBook.WebUI.ViewComponents.BlogViewComponents
 {
+
     public class _BlogDetailRecentBlogsComponentPartial : ViewComponent
     {
-        public IViewComponentResult Invoke() 
-        { 
-            return View(); 
+        private readonly IHttpClientFactory _httpClientFactory;
+
+        public _BlogDetailRecentBlogsComponentPartial(IHttpClientFactory httpClientFactory)
+        {
+            _httpClientFactory = httpClientFactory;
+        }
+
+        public async Task<IViewComponentResult> InvokeAsync()
+        {
+            var client = _httpClientFactory.CreateClient();
+            var responseMessage = await client.GetAsync("https://localhost:7208/api/Blogs/GetLastThreeBlogsWithAuthorsList");
+            if (responseMessage.IsSuccessStatusCode)
+            {
+                var jsondata = await responseMessage.Content.ReadAsStringAsync();
+                var values = JsonConvert.DeserializeObject<List<ResultLastThreeBlogWithAuthorDto>>(jsondata);
+                return View(values);
+            }
+            return View();
         }
     }
+
 }
