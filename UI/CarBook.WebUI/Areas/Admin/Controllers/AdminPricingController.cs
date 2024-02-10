3ed3907c2d9821bbcb4d942e08788b9deed677e3
ibrahimgltkn <ibrhm_gltkn74@hotmail.com>
2024-02-10 18:48:58 +0300

    Admin Pricing İşlemleri.

---
+++ UI/CarBook.WebUI/Areas/Admin/Controllers/AdminPricingController.cs
@@ -0,0 +1,97 @@
+﻿using CarBook.Dto.PricingDtos;
+using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json;
+using System.Text;
+
+namespace CarBook.WebUI.Areas.Admin.Controllers
+{
+    [Route("/Admin/AdminPricing")]
+    [Area("Admin")]
+    public class AdminPricingController : Controller
+    {
+        private readonly IHttpClientFactory _httpClientFactory;
+
+        public AdminPricingController(IHttpClientFactory httpClientFactory)
+        {
+            _httpClientFactory = httpClientFactory;
+        }
+
+        [Route("Index")]
+        public async Task<IActionResult> Index()
+        {
+            var client = _httpClientFactory.CreateClient();
+            var responseMessage = await client.GetAsync("https://localhost:7208/api/Pricings/");
+            if (responseMessage.IsSuccessStatusCode)
+            {
+                var jsonData = await responseMessage.Content.ReadAsStringAsync();
+                var values = JsonConvert.DeserializeObject<List<ResultPricingDto>>(jsonData);
+                return View(values);
+            }
+            return View();
+        }
+
+        [HttpGet]
+        [Route("CreatePricing")]
+        public IActionResult CreatePricing()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        [Route("CreatePricing")]
+        public async Task<IActionResult> CreatePricing(CreatePricingDto createPricingDto)
+        {
+            var client = _httpClientFactory.CreateClient();
+            var jsonData = JsonConvert.SerializeObject(createPricingDto);
+            StringContent content = new StringContent(jsonData, Encoding.UTF8, "application/json");
+            var responseMessage = await client.PostAsync("https://localhost:7208/api/Pricings/", content);
+            if (responseMessage.IsSuccessStatusCode)
+            {
+                return RedirectToAction("Index", "AdminPricing", new { area = "Admin" });
+            }
+            return View();
+        }
+
+        [Route("RemovePricing/{id}")]
+        public async Task<IActionResult> RemovePricing(int id)
+        {
+            var client = _httpClientFactory.CreateClient();
+            var responseMessage = await client.DeleteAsync($"https://localhost:7208/api/Pricings?id=" + id);
+            if (responseMessage.IsSuccessStatusCode)
+            {
+                return RedirectToAction("Index", "AdminPricing", new { area = "Admin" });
+            }
+            return View();
+        }
+
+        [HttpGet]
+        [Route("UpdatePricing/{id}")]
+        public async Task<IActionResult> UpdatePricing(int id)
+        {
+            var client = _httpClientFactory.CreateClient();
+            var responseMessage = await client.GetAsync($"https://localhost:7208/api/Pricings/{id}");
+            if (responseMessage.IsSuccessStatusCode)
+            {
+                var jsonData = await responseMessage.Content.ReadAsStringAsync();
+                var values = JsonConvert.DeserializeObject<UpdatePricingDto>(jsonData);
+                return View(values);
+            }
+            return View();
+        }
+
+        [HttpPost]
+        [Route("UpdatePricing/{id}")]
+        public async Task<IActionResult> UpdatePricing(UpdatePricingDto updatePricingDto)
+        {
+            var client = _httpClientFactory.CreateClient();
+            var jsonData = JsonConvert.SerializeObject(updatePricingDto);
+            StringContent content = new StringContent(jsonData, Encoding.UTF8, "application/json");
+            var responseMessage = await client.PutAsync("https://localhost:7208/api/Pricings/", content);
+            if (responseMessage.IsSuccessStatusCode)
+            {
+                return RedirectToAction("Index");
+            }
+            return View();
+        }
+    }
+}
