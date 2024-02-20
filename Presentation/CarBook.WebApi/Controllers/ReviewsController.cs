bf1f1303bbca89129c937aa87739c11ecc296ede
ibrahimgltkn <ibrhm_gltkn74@hotmail.com>
2024-02-21 00:42:28 +0300

    Yorumlar ve Değerlendirmeler Sayfası

---
+++ Presentation/CarBook.WebApi/Controllers/ReviewsController.cs
@@ -0,0 +1,27 @@
+﻿using CarBook.Application.Features.Mediator.Queries.LocationQueries;
+using CarBook.Application.Features.Mediator.Queries.ReviewQueries;
+using MediatR;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace CarBook.WebApi.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ReviewsController : ControllerBase
+    {
+        private readonly IMediator _mediator;
+
+        public ReviewsController(IMediator mediator)
+        {
+            _mediator = mediator;
+        }
+
+        [HttpGet("ReviewListByCarId")]
+        public async Task<IActionResult> ReviewListByCarId(int id)
+        {
+            var values = await _mediator.Send(new GetReviewByCarIdQuery(id));
+            return Ok(values);
+        }
+    }
+}
